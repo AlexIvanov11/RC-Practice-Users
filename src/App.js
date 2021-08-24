050c04338b254ba478c411736b65fb45b69efad7
Alex <Aleksandr.Ivanov@t-systems.com>
2021-08-24 23:19:17 +0300

    Module 8 is fully done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+
+import { AddUser } from "./components/Users/AddUser";
+import { UserList } from './components/Users/UserList';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const addUser = (username, age) => {
+    setUsers((prevUsersList) => {
+      return [...prevUsersList, {id: prevUsersList.length, username: username, age: age}]
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AddUser addUser={addUser}/>
+      <UserList users={users}/>
     </div>
   );
 }
